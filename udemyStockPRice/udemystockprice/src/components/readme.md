b79391667521c01205bc8616c1dda3617eaff2fc
Nicole Candiotti <nccandiotti@gmail.com>
2022-07-18 09:59:10 -0400

    new stock-price component + state  update for fetch

---
+++ udemyStockPRice/udemystockprice/src/components/readme.md
@@ -0,0 +1,10 @@
+# stock-price
+
+
+
+<!-- Auto Generated Below -->
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ udemyStockPRice/udemystockprice/src/components/stock-price.tsx
@@ -0,0 +1,40 @@
+import { Component, h, State } from '@stencil/core';
+
+@Component({
+  tag: 'stock-price',
+  styleUrl: 'stock-price.css',
+  shadow: true,
+  //stylse scoped to component bc shadow = true
+})
+export class StockPrice {
+  @State() stockPrice: number = 0;
+
+  onFetchStockPrice(event: Event) {
+    event.preventDefault();
+    console.log('submitted');
+    fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=LCF4V5FCK6KEPRW4')
+      .then(res => {
+        return res.json();
+      })
+      .then(parsedRes => {
+        this.stockPrice = parseInt(parsedRes['Global Quote']['05. price']);
+        console.log(this.stockPrice);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return [
+      <form onSubmit={this.onFetchStockPrice.bind(this)}>
+        <input id="stockSymbol" type="text" />
+        <button type="submit">Fetch</button>
+      </form>,
+      <div>
+        <p>Price: {this.stockPrice}</p>
+      </div>,
+    ];
+  }
+}
+// key LCF4V5FCK6KEPRW4
