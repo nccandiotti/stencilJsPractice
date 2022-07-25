ff6ab2dba58bac234fa95c30c0abc954c82a85d3
Nicole Candiotti <nccandiotti@gmail.com>
2022-07-25 11:48:57 -0400

    watch() and conditional render of tabs based on state

--- stencilYTTutorial/yt-component/src/components/my-card/my-card.tsx
+++ stencilYTTutorial/yt-component/src/components/my-card/my-card.tsx
@@ -1,8 +1,12 @@
-import { Component, h, Prop, State } from '@stencil/core';
+import { Component, h, Prop, State, Watch } from '@stencil/core';
 
 //Props from outside of component (can pass in index.html, like disabled for the button component) - add {mutable:true} to be able to change it from within component
 
 //state for changes within component
+
+// componentWillUpdate - updates component on state change ?
+
+//watch "watches" new value and old value
 
 @Component({
   tag: 'my-card',
@@ -12,16 +16,24 @@ import { Component, h, Prop, State } from '@stencil/core';
 export class MyCard {
   @Prop({ mutable: true }) name: string;
   @State() APIData: string;
+  @State() showReactTab: boolean = false;
+  @State() showStencilTab: boolean = false;
+
   changeState(): any {
     this.name = 'name has been updated';
     this.APIData = 'we have data from the api';
     this.showCard = true;
   }
+
   showCard: boolean = false;
 
-  componentWillUpdate(): void {
-    console.log('COMPONENTWILLUPDATE');
-  }
+  //   componentWillUpdate(): void {
+  //     console.log('COMPONENTWILLUPDATE');
+  //   }
+  //   @Watch('name')
+  //   watchHandler(newValue: boolean, oldValue: boolean) {
+  //     console.log('the New Value of name is ' + newValue + ' old value' + oldValue);
+  //   }
   render() {
     let stencilContent = (
       <div>
@@ -39,9 +51,7 @@ export class MyCard {
       <div>
         <div class="card-custom" id="react-div">
           Hello, from React <br></br> Live Users
-          <button onClick={() => this.componentWillUpdate()} class="btn-react small-btn">
-            Get React Users
-          </button>
+          <button class="btn-react small-btn">Get React Users</button>
           <br />
         </div>
       </div>
@@ -49,13 +59,16 @@ export class MyCard {
     let mainContent = (
       <div class="my-card-wrapper">
         <h2>Hi, I am {this.name}</h2>
-        <button class="btn-stencil">Stencil</button>
-        <button onClick={() => this.changeState()} class="btn-react">
+
+        <button onClick={() => (this.showStencilTab = !this.showStencilTab)} class="btn-stencil">
+          Stencil
+        </button>
+        <button onClick={() => (this.showReactTab = !this.showReactTab)} class="btn-react">
           React
         </button>
-
-        {reactContent}
-        {stencilContent}
+        {this.showCard ? <p>We have data from the API</p> : null}
+        {this.showReactTab ? reactContent : null}
+        {this.showStencilTab ? stencilContent : null}
       </div>
     );
 
