bf0cba2d042ef229db44d6bdc2dee9eb889939de
Nicole Candiotti <nccandiotti@gmail.com>
2022-07-25 08:32:15 -0400

    add props, state, componentWillUpdate

--- stencilYTTutorial/yt-component/src/components/my-card/my-card.tsx
+++ stencilYTTutorial/yt-component/src/components/my-card/my-card.tsx
@@ -1,6 +1,7 @@
 import { Component, h, Prop, State } from '@stencil/core';
 
 //Props from outside of component (can pass in index.html, like disabled for the button component) - add {mutable:true} to be able to change it from within component
+
 //state for changes within component
 
 @Component({
@@ -9,8 +10,18 @@ import { Component, h, Prop, State } from '@stencil/core';
   shadow: true,
 })
 export class MyCard {
-  @Prop() name: string;
-  @State()
+  @Prop({ mutable: true }) name: string;
+  @State() APIData: string;
+  changeState(): any {
+    this.name = 'name has been updated';
+    this.APIData = 'we have data from the api';
+    this.showCard = true;
+  }
+  showCard: boolean = false;
+
+  componentWillUpdate(): void {
+    console.log('COMPONENTWILLUPDATE');
+  }
   render() {
     let stencilContent = (
       <div>
@@ -28,7 +39,9 @@ export class MyCard {
       <div>
         <div class="card-custom" id="react-div">
           Hello, from React <br></br> Live Users
-          <button class="btn-react small-btn"> Get React Users</button>
+          <button onClick={() => this.componentWillUpdate()} class="btn-react small-btn">
+            Get React Users
+          </button>
           <br />
         </div>
       </div>
@@ -37,7 +50,9 @@ export class MyCard {
       <div class="my-card-wrapper">
         <h2>Hi, I am {this.name}</h2>
         <button class="btn-stencil">Stencil</button>
-        <button class="btn-react">React</button>
+        <button onClick={() => this.changeState()} class="btn-react">
+          React
+        </button>
 
         {reactContent}
         {stencilContent}
